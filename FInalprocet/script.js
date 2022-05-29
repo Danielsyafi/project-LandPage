a3f9ea91c5c5cc9ef075fb89f3a613460b9a86eb
Danielsyafi <100289426+Danielsyafi@users.noreply.github.com>
2022-05-29 21:25:30 +0700

    final
    note

---
+++ FInalprocet/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $('#show').click(function(){
+        $('#mod').show(500);
+    })
+    $('#close-button').click(function(){
+        $('#mod').hide(500);
+    })
+
+    $('.crs').click(function(){
+        $('.crsl').show(500);
+        
+    })
+})
